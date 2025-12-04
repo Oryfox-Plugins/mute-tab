a3d6622fe1f2c705439317f8c45fc82e65efd6fd
Semih Kaiser <git@oryfox.de>
2025-12-04 14:08:51 +0100

    fix: simplify tab mute logic

--- scripts/background.js
+++ scripts/background.js
@@ -2,10 +2,8 @@ chrome.commands.onCommand.addListener((command) => {
     if (command === "mute-tab") {
         chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
             let tab = tabs[0];
-            chrome.tabs.get(tab.id, (t) => {
-                let muted = !t.mutedInfo.muted;
-                chrome.tabs.update(tab.id, { muted: muted });
-            });
+            let muted = !tab.mutedInfo.muted;
+            chrome.tabs.update(tab.id, { muted: muted });
         });
     }
 });
